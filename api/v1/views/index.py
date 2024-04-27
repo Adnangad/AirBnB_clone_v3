2aed42deb42cb237388af4a40159e1213c7a0c54
Adnangad <gardobuyaadnan@gmail.com>
2024-04-27 02:50:26 -0700

    3-trying

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
-def return_status():
-    """returns status"""
-    return jsonify(status='OK')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
